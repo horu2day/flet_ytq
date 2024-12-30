b1fb5a426057bdf6333dc2880f01bea5aaac63c8
김민성 <kms3700@gmail.com>
2024-12-30 13:52:36 +0900

    vercel 맞추기

--- app.py
+++ app.py
@@ -8,14 +8,19 @@ def main(page: ft.Page):
             page.clean()  # 페이지 초기화
             youtube_app(page)  # 유튜브 앱 실행
         else:
-             page.add(ft.Text("인증 실패"))  # 인증 실패시 에러 메시지 출력
+            page.add(ft.Text("인증 실패"))  # 인증 실패시 에러 메시지 출력
     
     auth_app = AuthApp(auth_callback)
     auth_app.build(page)
-    
-def handler(event, context):
-    ft.app(target=main, view=ft.WEB_BROWSER)
 
+def handler(event, context):
+    # Vercel 환경에서 실행되는 경우
+    ft.app(target=main, view=ft.WEB_BROWSER, server_port=0) # 서버 포트 0으로 설정
+    
+    return {
+        'statusCode': 200,
+        'body': "Flet app initialized"
+    }
 if __name__ == "__main__":
-    ft.app(target=main, view=ft.WEB_BROWSER) # view=ft.WEB_BROWSER 추가
-    #ft.app(target=main)
+    # 개발 환경에서 실행되는 경우
+     ft.app(target=main, view=ft.WEB_BROWSER)
